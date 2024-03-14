2e1f7e6881e1e2beed4a9b461c56a62f31db1660
Unknow <Unknow>
2024-03-14 19:57:58 +0100

    Acceuil UI 75%

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header/header';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        {/* Le contenu principal de votre application va ici */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
